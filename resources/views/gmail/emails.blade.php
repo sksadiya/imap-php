864671e9464c8f44319704ad2c05aea24f69e122
sadiya <ssadiya.shaikh02@gmail.com>
2024-06-26 17:59:40 +0530

    Main commit

--- resources/views/gmail/emails.blade.php
+++ resources/views/gmail/emails.blade.php
@@ -1,50 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Emails</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- Bootstrap Bundle (JS and Popper.js) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
+
 <body>
     <div class="container mt-5">
         <nav class="nav nav-pills nav-fill" id="folderTabs">
             @foreach($folders as $key => $folder)
-            
-                <a class="nav-link {{ $key === 0 ? 'active' : '' }}" 
-                   id="{{ Illuminate\Support\Str::slug($folder['name']) }}-tab" 
-                   data-toggle="tab" 
-                   href="#{{ Illuminate\Support\Str::slug($folder['name']) }}-content" 
-                   role="tab" 
-                   aria-controls="{{ Illuminate\Support\Str::slug($folder['name']) }}-content" 
-                   aria-selected="{{ $key === 0 ? 'true' : 'false' }}">
-                   {{ $folder['name'] }}
+
+                <a class="nav-link {{ $key === 0 ? 'active' : '' }}"
+                    id="{{ Illuminate\Support\Str::slug($folder['name']) }}-tab" data-toggle="tab"
+                    href="#{{ Illuminate\Support\Str::slug($folder['name']) }}-content" role="tab"
+                    aria-controls="{{ Illuminate\Support\Str::slug($folder['name']) }}-content"
+                    aria-selected="{{ $key === 0 ? 'true' : 'false' }}">
+                    {{ $folder['name'] }}
                 </a>
             @endforeach
         </nav>
 
-        <div class="tab-content" id="folderContent">
+        <div class="tab-content py-4" id="folderContent">
             @foreach($folders as $key => $folder)
-                <div class="tab-pane fade {{ $key === 0 ? 'show active' : '' }}" 
-                     id="{{ Illuminate\Support\Str::slug($folder['name']) }}-content" 
-                     role="tabpanel" 
-                     aria-labelledby="{{ Illuminate\Support\Str::slug($folder['name']) }}-tab">
-                     
-                     <!-- Place your messages content here -->
-                     <h4>{{ $folder['name'] }} Messages</h4>
-                     <ul>
-                         @foreach($messages[$folder['name']] as $message)
-                             <li>{{ $message['subject'] }} - {{ $message['from'] }} - {{ $message['date'] }}</li>
-                         @endforeach
-                     </ul>
-                     
+                <div class="tab-pane fade {{ $key === 0 ? 'show active' : '' }}"
+                    id="{{ Illuminate\Support\Str::slug($folder['name']) }}-content" role="tabpanel"
+                    aria-labelledby="{{ Illuminate\Support\Str::slug($folder['name']) }}-tab">
+                    <div class="card">
+                        <div class="card-body">
+                            <!-- Place your messages content here -->
+                            <h4>{{ $folder['name'] }} Messages</h4>
+                            <ul class="list-group">
+                                @foreach($messages[$folder['name']] as $message)
+                                    <li class="list-group-item">{{ $message['subject'] }} - {{ $message['from'] }} -
+                                        {{ $message['date']->format('d-M-Y H:i:s') }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
+
                 </div>
             @endforeach
         </div>
@@ -52,7 +58,7 @@
 
     <!-- JavaScript to handle tab switching -->
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $('#folderTabs a').on('click', function (e) {
                 e.preventDefault();
                 $(this).tab('show');
@@ -60,4 +66,5 @@
         });
     </script>
 </body>
+
 </html>
